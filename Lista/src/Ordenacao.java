5ada8a2aaaa794fa110752dd82a1754f26470814
leo <leogomesanholetto@gmail.com>
2026-02-07 14:29:00 -0300

    Aula nova

--- Lista/src/Ordenacao.java
+++ Lista/src/Ordenacao.java
@@ -195,7 +195,7 @@ public class Ordenacao {
     }
 
     public void count(int[] arr, int exp){
-        int conta[10];
+        int[] conta = new int[10];
         for(int i=0;i<arr.length;i++){
             conta[(arr[i]/exp)%10]++;
         }
